4029a2352577ce632e27b7f1fa9ba8adda0c8fcf
Ufuk Bakan <println.ufukbakan@gmail.com>
2024-02-11 04:13:07 +0300

    feat init prime react

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,14 @@
-import { render } from 'preact'
-import { App } from './app.tsx'
-import './index.css'
+import { render } from 'preact';
+import 'primeicons/primeicons.css';
+import "primereact/resources/themes/viva-light/theme.css";
+import { PrimeReactProvider } from "primereact/api";
+import { RecoilRoot } from 'recoil';
+import { App } from './app.tsx';
 
-render(<App />, document.getElementById('app')!)
+render(
+    <RecoilRoot>
+        <PrimeReactProvider value={{ unstyled: false, inputStyle: "filled" }}>
+            <App />
+        </PrimeReactProvider>
+    </RecoilRoot>
+    , document.getElementById('app')!)
