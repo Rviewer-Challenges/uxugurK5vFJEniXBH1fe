1f4a9ef723dec89777d29c4a7012fe48d5f9e35d
Ufuk Bakan <println.ufukbakan@gmail.com>
2024-02-11 22:01:58 +0300

    refactor: move business logic

--- src/pages/Convertor.tsx
+++ src/pages/Convertor.tsx
@@ -2,16 +2,12 @@ import { Box, Button, Flex } from "@mantine/core";
 import { useRecoilState } from "recoil";
 import { selectedCountry1atom, selectedCountry2atom } from "../atoms/countryAtoms";
 import CountrySelector from "../components/CountrySelector";
-import { gdpList } from "../data/countriesGdp";
+import { gdpService } from "../service/gdpService";
 
 export default function Convertor() {
 
     const [country1, setCountry1] = useRecoilState(selectedCountry1atom);
     const [country2, setCountry2] = useRecoilState(selectedCountry2atom);
-
-    const gdp1 = gdpList[country1];
-    const gdp2 = gdpList[country2];
-    const divisionConstant = 5;
 
     function swap() {
         setCountry1(country2);
@@ -31,13 +27,13 @@ export default function Convertor() {
                 </Flex>
 
                 <Flex direction="column">
-                    <span className="lg-text" children={gdp1 / gdp2} />
+                    <span className="lg-text" children={gdpService.convert(country1, country2)} />
                     <CountrySelector countryAtom={selectedCountry2atom} />
                 </Flex>
             </Flex>
             <Flex direction="row" gap="0">
-                <Box w="50%" px={2}>{"💶".repeat(Math.floor(gdp1 / divisionConstant))}</Box>
-                <Box w="50%" px={2}>{"💶".repeat(Math.floor(gdp2 / divisionConstant))}</Box>
+                <Box w="50%" px={2}>{gdpService.visualize(country1)}</Box>
+                <Box w="50%" px={2}>{gdpService.visualize(country2)}</Box>
             </Flex>
         </Flex>
     )
