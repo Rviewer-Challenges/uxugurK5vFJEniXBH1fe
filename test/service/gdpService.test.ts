ff6e46ee4f276146461cbb714e651c9801d359a4
Ufuk Bakan <println.ufukbakan@gmail.com>
2024-02-11 22:07:59 +0300

    feat: service test

---
+++ test/service/gdpService.test.ts
@@ -0,0 +1,17 @@
+import { expect, it } from "vitest";
+import { GdpCountry } from "../../src/data/countriesGdp";
+import { gdpService } from "../../src/service/gdpService";
+
+it("Test visualize Gdp", () => {
+    const country: GdpCountry = "Turkey";
+    gdpService.visualDivisionConstant = 5;
+    expect(gdpService.visualize(country)).to.contain("💶".repeat(190));
+    gdpService.visualDivisionConstant = 10;
+    expect(gdpService.visualize(country)).to.contain("💶".repeat(95));
+});
+
+it("Test GDP equality convertor", () => {
+    const country1: GdpCountry = "United States of America";
+    const country2: GdpCountry= "China";
+    expect(gdpService.convert(country1, country2)).to.be.eq("1.282");
+});
