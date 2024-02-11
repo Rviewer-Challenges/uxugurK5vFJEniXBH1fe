1f4f4d6e263c3c60a16fe8e56cc2e2542f2cf2c0
Ufuk Bakan <println.ufukbakan@gmail.com>
2024-02-11 18:46:11 +0300

    feat: changed default values

--- src/atoms/countryAtoms.ts
+++ src/atoms/countryAtoms.ts
@@ -1,12 +1,16 @@
 import { atom } from "recoil";
-import { GdpCountry, gdpList } from "../data/countriesGdp";
+import { GdpCountry, gdpCountries } from "../data/countriesGdp";
+
+function equal<T extends V, V>(target: T) {
+    return (value: V) => value === target;
+}
 
 export const selectedCountry1atom = atom<GdpCountry>({
     key: "selectedCountry1",
-    default: Object.keys(gdpList).at(0)
+    default: gdpCountries.find(equal("Russia"))
 })
 
 export const selectedCountry2atom = atom<GdpCountry>({
     key: "selectedCountry2",
-    default: Object.keys(gdpList).at(1)
+    default: gdpCountries.find(equal("Turkey"))
 })
